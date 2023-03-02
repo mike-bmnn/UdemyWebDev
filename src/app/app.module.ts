e9a877aed99b77fe7a8a322c0c5e1313ce344762
Mike <theonlybuschi@gmail.com>
2023-03-02 17:32:50 +0100

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {HeaderComponent} from "./header/header.component";
 import {PostListComponent} from "./posts/post-list/post-list.component";
 import {MatExpansionModule} from "@angular/material/expansion";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import {PostCreateComponent} from "./posts/post-create/post-create.component";
 import {FormsModule} from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {PostService} from "./posts/post.service";
+
 
 @NgModule({
   declarations: [
@@ -29,7 +31,8 @@ import {PostService} from "./posts/post.service";
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
 
   ],
   providers: [PostService],
